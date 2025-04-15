bd9af7a00cf92808f3ef314baf13983410915d81
axbuglak <alexeybuglak24@gmail.com>
2025-04-15 23:29:16 +0200

    initil step 3

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const { readXlsx, Proxies } = require('./lib/common');
 const itemsIds = require('./static/NameID.json');
 const SteamItem = require('./lib/steamItem');
+const AccountsManages = require('./lib/account');
 const path = require('path');
 const console = require('./lib/logger.js');
 
@@ -11,12 +12,14 @@ const proxyPathOne = path.join(process.cwd(), 'static/accounts_stage1.xlsx');
 const proxyPathTwo = path.join(process.cwd(), 'static/accounts_stage2.xlsx');
 const proxiesOne = new Proxies(proxyPathOne);
 const proxiesTwo = new Proxies(proxyPathTwo);
+const accounts = new AccountsManages();
 const items = readXlsx(itemsPath);
 for (const item of items) {
   const { Name: name, Float: float, Price: price, Sleep: sleep } = item;
   const id = itemsIds.find(
     (item) => item.market_hash_name === name,
   ).commodityID;
+  const getAccount = () => accounts.getAccount();
   const steamItem = new SteamItem({
     name,
     float,
@@ -26,6 +29,7 @@ for (const item of items) {
     proxiesOne,
     proxiesTwo,
     console,
+    getAccount,
   });
   steamItem.stepOne();
 }
