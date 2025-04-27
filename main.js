3b90a5f50fc5ca34c10edffc48157b8bd608f0b0
axbuglak <alexeybuglak24@gmail.com>
2025-04-27 23:07:32 +0200

    upd

--- main.js
+++ main.js
@@ -14,36 +14,41 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const USER_ID = process.env.USER_ID;
 const bot = new TelegramBot(BOT_TOKEN, USER_ID);
 
-const itemsPath = path.join(process.cwd(), 'static/items.xlsx');
-const proxyPathOne = path.join(process.cwd(), 'static/accounts_stage1.xlsx');
-const proxyPathTwo = path.join(process.cwd(), 'static/accounts_stage2.xlsx');
-const accountsBuyPath = path.join(process.cwd(), 'static/accounts_buyer.xlsx');
-const proxiesOne = new Proxies(proxyPathOne);
-const proxiesTwo = new Proxies(proxyPathTwo);
-const items = readXlsx(itemsPath);
-const accountsBuy = readXlsx(accountsBuyPath);
-const accounts = new AccountsManages({
-  accountsCookies: accountsBuy,
-  delay: 5, // delay in s
-  critBalance: 22, // critical balance point
-});
-for (const item of items) {
-  const { Name: name, Float: float, Price: price, Sleep: sleep } = item;
-  const id = itemsIds.find(
-    (item) => item.market_hash_name === name,
-  ).commodityID;
-  const getAccount = () => accounts.getAccount();
-  const steamItem = new SteamItem({
-    name,
-    float,
-    price,
-    sleep,
-    id,
-    proxiesOne,
-    proxiesTwo,
-    console,
-    getAccount,
-    sendTgMessage: async (m) => await bot.sendMessage(m),
+(async () => {
+  const itemsPath = path.join(process.cwd(), 'static/items.xlsx');
+  const proxyPathOne = path.join(process.cwd(), 'static/accounts_stage1.xlsx');
+  const proxyPathTwo = path.join(process.cwd(), 'static/accounts_stage2.xlsx');
+  const accountsBuyPath = path.join(
+    process.cwd(),
+    'static/accounts_buyer.xlsx',
+  );
+  const proxiesOne = new Proxies(proxyPathOne);
+  const proxiesTwo = new Proxies(proxyPathTwo);
+  const items = readXlsx(itemsPath);
+  const accountsBuy = readXlsx(accountsBuyPath);
+  const accounts = new AccountsManages({
+    delay: 5, // delay in s
+    critBalance: 0, // critical balance point
   });
-  steamItem.stepOne();
-}
+  await accounts.setup(accountsBuy);
+  for (const item of items) {
+    const { Name: name, Float: float, Price: price, Sleep: sleep } = item;
+    const id = itemsIds.find(
+      (item) => item.market_hash_name === name,
+    ).commodityID;
+    const getAccount = () => accounts.getAccount();
+    const steamItem = new SteamItem({
+      name,
+      float,
+      price,
+      sleep,
+      id,
+      proxiesOne,
+      proxiesTwo,
+      console,
+      getAccount,
+      sendTgMessage: async (m) => await bot.sendMessage(m),
+    });
+    steamItem.stepOne();
+  }
+})();
